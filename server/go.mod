b52eb377618a82ca04cabf27e6b778f7305bd758
LAIS CARVALHO DE LIMA <lais.carvalho@g.globo>
2022-06-22 15:49:05 -0300

    Fix docker for golang server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,3 @@
 module main
 
-go 1.17
+go 1.16
--- server/main.go
+++ server/main.go
@@ -4,20 +4,30 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
-var users []string
+type Response struct {
+	Message  string `json:message`
+	Datetime string `json:datetime`
+}
 
-func listUsers(w http.ResponseWriter, req *http.Request) {
+func message(w http.ResponseWriter, req *http.Request) {
+	loc, _ := time.LoadLocation("America/Sao_Paulo")
+	currentTime := time.Now().In(loc)
+	timeFormatted := currentTime.Format("2006-01-02 3:4:5")
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
-	users = []string{"Lais", "Asael"}
-	j, _ := json.Marshal(users)
+	resp := &Response{
+		Message:  "Hello world!",
+		Datetime: timeFormatted,
+	}
+	j, _ := json.Marshal(resp)
 	w.Write(j)
 }
 
 func main() {
-	http.HandleFunc("/list", listUsers)
+	http.HandleFunc("/", message)
 	http.ListenAndServe(":8082", nil)
 	log.Print("Server is listening on port :8082")
 }
